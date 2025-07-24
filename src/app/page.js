8905ad59b5655d3ad6d454ef887cde0ae80fbc5c
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-24 19:46:36 +0530

    uimodified: for signin signout buttons

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,26 @@
-// src/app/page.tsx
-import { getCurrentUser } from "../lib/auth";
-import ClientAuthForm from "./ClientAuthForm";
-import LogoutButton from "./LogoutButton";
+// src/app/page.tsx (or any layout)
 
-export default async function Home() {
-  const user = await getCurrentUser();
+import { auth } from "../lib/auth"; // we added this earlier
+import { SignInButton, SignOutButton } from "./auth-buttons";
+
+export default async function HomePage() {
+  const session = await auth();
+
+  console.log({session})
 
   return (
-    <main className="p-10">
-      {user ? (
-        <div>
-          <h1 className="text-2xl font-bold">👋 Welcome, {user.email}</h1>
-          <LogoutButton />
-        </div>
+    <main className="p-4">
+      {session?.user ? (
+        <>
+          <h1>Welcome, {session.user.name}</h1>
+          <p>Your email: {session.user.email}</p>
+          <img className="w-10 h-10" src={session.user.image} alt="users-profile"/>
+          <SignOutButton />
+        </>
       ) : (
         <>
-          <h1 className="text-2xl font-bold mb-4">Not Logged In</h1>
-          <ClientAuthForm />
+          <h1>You are not logged in</h1>
+          <SignInButton />
         </>
       )}
     </main>
