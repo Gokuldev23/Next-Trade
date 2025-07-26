4787df6292aa8209096e6227865b5f6847a00b38
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-26 20:56:07 +0530

    DEBUG: logging db url temporarily commented

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
 import { handlers } from "../../../../lib/auth"; // Referring to the auth.ts we just created
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+// console.log("DATABASE_URL:", process.env.DATABASE_URL);
 export const { GET, POST } = handlers;
