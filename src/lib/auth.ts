766170a45de09f945f9dd26a21a1ce2d435e0696
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-26 20:17:00 +0530

    fix: Connection Pool initialized inside NextAuth handler

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,23 @@
 import NextAuth from "next-auth";
 import NeonAdapter from "@auth/neon-adapter";
 import GitHub from "next-auth/providers/github";
-
 import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 20,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
 
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: NeonAdapter(pool),
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: "database",
-    maxAge: 60,
-  },
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  // Create a `Pool` inside the request handler.
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+  return {
+    adapter: NeonAdapter(pool),
+    providers: [
+      GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      }),
+    ],
+    session: {
+      strategy: "database",
+      maxAge: 60,
+    },
+  }
 });
