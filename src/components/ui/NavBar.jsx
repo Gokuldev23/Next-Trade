a1847d2f3ecc69efbd5684e8c049fab7cb0453a7
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-30 14:15:46 +0530

    uiadded: NextTrade Logo and Defaultt fallback logo added on NavBar

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import UserProfile from "../ui/UserProfile"
+import { AlignRight } from "lucide-react";
 
 export default async function NavBar() {
   return (
-    <div className='h-20 border'>
-        <p>Some</p>
+    <div className="p-4 bg-neutral-800 rounded-lg flex justify-between pr-8 items-center">
+      <div className="flex gap-x-4 items-center">
+        <button popoverTarget="side-bar" popoverTargetAction="toggle">
+          <AlignRight />
+        </button>
+        <Image
+          className="w-12 rounded-full"
+          src={"/NextTradeLogo.png"}
+          width={100}
+          height={100}
+          alt="Next Trade Branding Logo"
+        />
+      </div>
+      <UserProfile/>
     </div>
-  )
+  );
 }
